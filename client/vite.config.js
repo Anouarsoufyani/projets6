9a372c9158fb1fd24bcfe7f5e8c7efcdc41e7e5c
Anouar <anouar.soufyani@gmail.com>
2025-04-10 00:08:42 +0200

    change url server

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,14 +7,14 @@ import dotenv from "dotenv";
 dotenv.config();
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true
-      }
-    }
-  }
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 3000,
+        proxy: {
+            "/api": {
+                target: "https://projets6.onrender.com",
+                changeOrigin: true,
+            },
+        },
+    },
 });

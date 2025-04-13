4fd5b77051b91cae1ee1a6da52d745e2e5602b2a
Anouar <anouar.soufyani@gmail.com>
2025-04-13 14:12:03 +0200

    grosse maj, hooks modulaires, remove inutiles

--- New folder/fqhqd/src/Pages/Admin/GestionCommandePage.jsx
+++
@@ -1,5 +0,0 @@
-const GestionCommandePage = () => {
-  return <div>GestionCommandePage</div>
-}
-
-export default GestionCommandePage
--- front/src/Pages/Admin/GestionCommandePage.jsx
+++
@@ -1,5 +0,0 @@
-const GestionCommandePage = () => {
-  return <div className="w-full h-full bg-gradient-to-br from-emerald-50 to-teal-100 p-6">GestionCommandePage</div>
-}
-
-export default GestionCommandePage

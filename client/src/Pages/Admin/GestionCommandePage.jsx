5b3499f9c5ea8a41f78bf88ec1d6b2b5d76424b8
Anouar <anouar.soufyani@gmail.com>
2025-04-29 01:28:49 +0200

    gestion Commandes done

--- client/src/Pages/Admin/GestionCommandePage.jsx
+++ client/src/Pages/Admin/GestionCommandePage.jsx
@@ -850,7 +850,7 @@ const GestionCommandePage = () => {
 
             {/* Edit Modal */}
             {isEditModalOpen && selectedCommande && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+                <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
                     <div className="bg-white rounded-xl shadow-xl max-w-2xl w-full">
                         <div className="p-6">
                             <div className="flex justify-between items-center mb-6">
